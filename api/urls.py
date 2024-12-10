b7c18385ee2a141727a9163c13211ae0543c507e
otto0521 <chara_chi@SEIGI30W10.localdomain>
2024-12-11 03:59:23 +0900

    redirect top

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
+    path('', lambda request: redirect('robot_dashboard')),
     path('top/', views.robot_dashboard, name='robot_dashboard'),
     path('signup/', views.signup, name='signup'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
